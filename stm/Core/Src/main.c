4f00074f3b052d1b1d0998875e71b98fd1f4d626
brew02 <brodiea2005@gmail.com>
2025-04-24 04:51:08 -0600

    Add joystick handling and printing (render frame needs to be fixed)

--- stm/Core/Src/main.c
+++ stm/Core/Src/main.c
@@ -32,13 +32,11 @@ void enableClocks() {
   BITSET(RCC->APB1ENR1, 28); // Power interface clock enable
   BITSET(RCC->APB1ENR1, 1);  // TIM3 clock enable
 
-  BITSET(RCC->AHB1ENR, 2); // DMAMUX1 clock enable
-  BITSET(RCC->AHB1ENR, 0); // DMA1 clock enable
-
   BITSET(RCC->AHB2ENR, 13); // ADC clock enable
   BITSET(RCC->AHB2ENR, 6);  // GPIOG clock enable
   BITSET(RCC->AHB2ENR, 4);  // GPIOE clock enable
   BITSET(RCC->AHB2ENR, 2);  // GPIOC clock enable
+  BITSET(RCC->AHB2ENR, 0);  // GPIOA clock enable
 
   BITSET(RCC->APB1ENR2, 0); // LPUART1 clock enable
 
@@ -66,8 +64,8 @@ void enableClocks() {
   GPIOE 12              <-> J2.13 (LCD SPI CS)
   GPIOE 11              <-> J4.31 (LCD RS)
   GPIOE 10              <-> J2.17 (LCD RST)
-  GPIOC 1               <-> J1.2  (Joystick Hor(x))
-  GPIOC 0               <-> J3.26 (Joystick Ver(y))
+  GPIOA 5               <-> J1.2  (Joystick Hor(x))
+  GPIOA 4               <-> J3.26 (Joystick Ver(y))
   GPIOE 7               <-> J1.5  (Joystick Select)
   GPIOE 6               <-> J4.32 (Button 2)
   GPIOE 5               <-> J4.33 (Button 1)
@@ -135,11 +133,11 @@ void initGPIOs() {
   BITCLEAR(GPIOC->MODER, 27); // Set GPIOC 13 to input
   BITCLEAR(GPIOC->MODER, 26);
 
-  BITSET(GPIOC->MODER, 3); // Set GPIOC 1 to analog input
-  BITSET(GPIOC->MODER, 2);
+  BITSET(GPIOA->MODER, 11); // Set GPIOA 5 to analog input
+  BITSET(GPIOA->MODER, 10);
 
-  BITSET(GPIOC->MODER, 1); // Set GPIOC 0 to analog input
-  BITSET(GPIOC->MODER, 0);
+  BITSET(GPIOA->MODER, 9); // Set GPIOA 4 to analog input
+  BITSET(GPIOA->MODER, 8);
 }
 
 /**
@@ -183,70 +181,47 @@ void initLPUART1() {
   NVIC_EnableIRQ(LPUART1_IRQn);      // Enable LPUART1 IRQ
 }
 
-uint16_t joystick[2] = {0, 0};
-
 void initADC1() {
-  DMA1->IFCR |= 0b1111;            // Clear all interrupts for channel 1
-  BITCLEAR(DMA1_Channel1->CCR, 0); // Disable channel 1
-  while (BITCHECK(DMA1_Channel1->CCR, 0) == 1)
-    ;                   // Wait for channel 1 to be disabled
-  DMA1->IFCR |= 0b1111; // Clear all interrupts for channel 1
-
-  DMA1_Channel1->CPAR = (volatile unsigned long)&ADC1->DR;
-  DMA1_Channel1->CM0AR = (volatile unsigned long)joystick;
-  DMA1_Channel1->CNDTR = 2;
-
-  BITCLEAR(DMA1_Channel1->CCR, 14); // Disable memory-to-memory mode
-  BITCLEAR(DMA1_Channel1->CCR, 11); // Set 16-bit memory size
-  BITSET(DMA1_Channel1->CCR, 10);
-  BITCLEAR(DMA1_Channel1->CCR, 9); // Set 16-bit peripheral size
-  BITSET(DMA1_Channel1->CCR, 8);
-  BITSET(DMA1_Channel1->CCR, 7);   // Enable increment memory mode
-  BITSET(DMA1_Channel1->CCR, 5);   // Enable circular mode
-  BITCLEAR(DMA1_Channel1->CCR, 4); // Read from peripheral to memory
-
-  DMAMUX1_Channel0->CCR = 5; // Triggered by ADC
-
-  BITSET(DMA1_Channel1->CCR, 0); // Enable channel 1
+  // Check if the ADC is enabled
+  if (BITCHECK(ADC1->CR, 0) == 1) {
+    BITSET(ADC1->CR, 1); // Disable ADC1
+    // Wait for the ADC to disable
+    while (BITCHECK(ADC1->CR, 0) == 1)
+      ;
+  }
 
   BITCLEAR(ADC1->CR, 29); // Disable ADC deep-power down
   BITSET(ADC1->CR, 28);   // Enable ADC voltage regulator
 
   delayMS(10);
 
-  BITSET(ADC1->CFGR, 13); // Enable continuous conversion mode
-  BITSET(ADC1->CFGR, 1);  // Use DMA circular mode
-  BITSET(ADC1->CFGR, 0);  // Enable DMA
+  BITSET(ADC12_COMMON->CCR, 21); // Divide ADC clocks by 32
+  BITCLEAR(ADC12_COMMON->CCR, 20);
+  BITCLEAR(ADC12_COMMON->CCR, 19);
+  BITCLEAR(ADC12_COMMON->CCR, 18);
 
-  BITCLEAR(ADC1->SQR1, 16); // Read channel 2
-  BITCLEAR(ADC1->SQR1, 15);
-  BITCLEAR(ADC1->SQR1, 14);
-  BITSET(ADC1->SQR1, 13);
-  BITCLEAR(ADC1->SQR1, 12);
-
-  BITCLEAR(ADC1->SQR1, 10); // Read channel 1
-  BITCLEAR(ADC1->SQR1, 9);
-  BITCLEAR(ADC1->SQR1, 8);
-  BITCLEAR(ADC1->SQR1, 7);
-  BITSET(ADC1->SQR1, 6);
-
-  BITCLEAR(ADC1->SQR1, 3); // Read 2 channels
-  BITCLEAR(ADC1->SQR1, 2);
-  BITCLEAR(ADC1->SQR1, 1);
-  BITSET(ADC1->SQR1, 0);
-
-  BITSET(ADC1->SMPR1, 8); // Sample channel 2 for 92.5 ADC clock cycles
-  BITCLEAR(ADC1->SMPR1, 7);
-  BITSET(ADC1->SMPR1, 6);
-  BITSET(ADC1->SMPR1, 5); // Sample channel 1 for 92.5 ADC clock cycles
-  BITCLEAR(ADC1->SMPR1, 4);
-  BITSET(ADC1->SMPR1, 3);
+  BITSET(ADC1->ISR, 0); // Clear ADRDY flag
 
   BITSET(ADC1->CR, 0);                // Enable ADC1
   while (BITCHECK(ADC1->ISR, 0) == 0) // Wait for ADC1 to be ready
     ;
 
-  BITSET(ADC1->CR, 2); // Start regular conversions of ADC1
+  BITSET(ADC1->ISR, 0); // Clear ADRDY flag
+
+  BITCLEAR(ADC1->JSQR, 18); // Read channel 10 (GPIOA 5)
+  BITSET(ADC1->JSQR, 17);
+  BITCLEAR(ADC1->JSQR, 16);
+  BITSET(ADC1->JSQR, 15);
+  BITCLEAR(ADC1->JSQR, 14);
+
+  BITCLEAR(ADC1->JSQR, 12); // Read channel 9 (GPIOA 4)
+  BITSET(ADC1->JSQR, 11);
+  BITCLEAR(ADC1->JSQR, 10);
+  BITCLEAR(ADC1->JSQR, 9);
+  BITSET(ADC1->JSQR, 8);
+
+  BITCLEAR(ADC1->JSQR, 1); // Read two injected channels
+  BITSET(ADC1->JSQR, 0);
 }
 
 /**
@@ -268,8 +243,10 @@ int main() {
   // White
   textColor = color24to16(0xFF, 0xFF, 0xFF);
   renderString(0, 0, "Hello", textColor, bgColor);
+  renderString(0, 1, "World", textColor, bgColor);
 
-  char buf[100];
+  uint16_t joystick[2];
+  char buffer[100];
   while (1) {
     //  For testing in Python
     //     if (flag == 1) {
@@ -284,6 +261,23 @@ int main() {
       // renderString(0, 0, buf, text, bg);
       flag = 0;
     }
+
+    BITSET(ADC1->ISR, 6); // Clear the end of injected channel sequence flag
+    BITSET(ADC1->CR, 3);  // Start an injected conversion
+    // Wait for the injected sequence to finish
+    while (BITCHECK(ADC1->ISR, 6) == 0)
+      ;
+    // Read the vertical joystick position
+    joystick[0] = ADC1->JDR1 & 0xFFF;
+    // Read the horizontal joystick position
+    joystick[1] = ADC1->JDR2 & 0xFFF;
+
+    memset(buffer, 0, sizeof(buffer));
+    sprintf(buffer, "V: %0*u", 4, joystick[0]);
+    renderString(0, 2, buffer, textColor, bgColor);
+    memset(buffer, 0, sizeof(buffer));
+    sprintf(buffer, "H: %0*u", 4, joystick[1]);
+    renderString(0, 3, buffer, textColor, bgColor);
 
     delayMS(100);
   }
